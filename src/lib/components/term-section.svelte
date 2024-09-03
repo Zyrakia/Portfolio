5b84554d6c04a620ba1ce57482fb8e76cf2403a5
Zyrakia <ole.lammers@pm.me>
2024-09-03 16:27:16 -0700

    docs: changed spacing and indicators for sections

--- src/lib/components/term-section.svelte
+++ src/lib/components/term-section.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+	import TermText from './term-text.svelte';
+
 	export let title: string | undefined = undefined;
 </script>
 
-<div>
+<div class="section">
 	{#if title}
-		<h2>{title}</h2>
+		<TermText text={title} lineIndicator="|" />
 	{/if}
 
-	<slot />
+	<div class="content" style="padding-left: {title ? '1.25rem' : '0'}">
+		<slot />
+	</div>
 </div>
 
 <style>
-	div {
-		text-indent: 1em;
+	.section {
 		display: flex;
 		flex-direction: column;
 	}
--- src/lib/components/term-text.svelte
+++ src/lib/components/term-text.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	export let text: string;
+	export let lineIndicator = '$';
 </script>
 
-<span class="terminal-text"><span class="line-indicator">$</span> {text}</span>
+<p class="terminal-text">
+	<span class="line-indicator">{lineIndicator}</span>
+	<span class="text-content">
+		{text}
+	</span>
+</p>
 
 <style>
 	.line-indicator {
 		color: darkseagreen;
-		font-weight: bold;
+		font-weight: lighter;
 	}
 </style>
