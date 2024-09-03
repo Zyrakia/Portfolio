8f4ebaec5b9f17be23141b612e6974e47a3cb46b
Zyrakia <ole.lammers@pm.me>
2024-09-03 16:26:12 -0700

    fix: ensured typewriter reacts to text updates

---
+++ src/lib/components/typewriter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onDestroy } from 'svelte';
+
+	export let text: string;
+	export let renderAsHtml = false;
+	export let charPerSecond = 15;
+
+	let renderedText = '';
+
+	const computeNextRenderedText = () => {
+		let targetRenderedChars = 1;
+		for (let i = 0; i < text.length; i++) {
+			targetRenderedChars++;
+			if (text[i] !== renderedText[i]) break; 
+		}
+
+		return text.slice(0, Math.min(text.length, targetRenderedChars));
+	};
+
+	const renderTick = () => {
+		if (text === renderedText) return;
+		renderedText = computeNextRenderedText();
+	};
+
+	let renderTicker: number | undefined;
+	const startRender = (interval: number) => {
+		if (!browser) return;
+		stopRender();
+		renderTicker = setInterval(renderTick, interval);
+	};
+
+	const stopRender = () => {
+		if (renderTicker === undefined) return;
+		clearInterval(renderTicker);
+		renderTicker = undefined;
+	};
+
+	$: startRender(1000 / charPerSecond);
+	onDestroy(stopRender);
+</script>
+
+{#if renderAsHtml}
+	{@html renderedText}
+{:else}
+	{renderedText}
+{/if}
