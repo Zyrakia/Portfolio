7eb273b9e88d11ba842e275fca60a0e8a214b965
Zyrakia <ole.lammers@pm.me>
2024-10-01 21:12:47 -0700

    feat: supported not specifying protocol in env URL

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,10 @@ import { z } from 'zod';
 export const env = createEnv({
 	clientPrefix: 'VITE_',
 	client: {
-		VITE_API_URL: z.string().url(),
+		VITE_API_URL: z.string().transform((v) => {
+			if (v.startsWith('https://') || v.startsWith('http://')) return v;
+			return `https://${v}`;
+		}),
 	},
 	runtimeEnv: import.meta.env,
 	emptyStringAsUndefined: true,
